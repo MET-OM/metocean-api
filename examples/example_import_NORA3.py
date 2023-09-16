df835c6a961a27449962909cf91ce05fb3c780f3
Konstantinos Christakos <67804784+KonstantinChri@users.noreply.github.com>
2023-09-16 20:47:38 +0200

    Update example_import_NORA3.py

--- examples/example_import_NORA3.py
+++ examples/example_import_NORA3.py
@@ -4,9 +4,8 @@ from metocean_api import ts
 ts = ts.TimeSeries(lon=1.320, lat=53.324,start_time='2022-10-01', end_time='2022-12-31' , product='NORA3_wind_wave')
 
 # Import data from thredds.met.no and save it as csv
-ts.import_ts(save_csv=True)
+ts.import_data(save_csv=True)
 
 # Load data from a local csv-file
-#ts.load_ts(local_file=ts.datafile)
-
+#ts.load_data(local_file=ts.datafile)
 
