dbe860e8c52908aa75ef8b608dd2765a29f25f0c
Konstantinos Christakos <67804784+KonstantinChri@users.noreply.github.com>
2023-09-16 21:25:34 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
