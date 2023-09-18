878b5667d55d5800096498ddbfe07a9e653190ff
KonstantinChri <kchrist87@gmail.com>
2023-09-18 12:50:44 +0200

    update

---
+++ metocean_api/ts/__init__.py
@@ -0,0 +1,17 @@
+"""
+This is the TimeSeries-module that is responsible for extracting TimeSeries-object.
+
+The Extraction-object and the abstract classes used by the other sub-
+modules are defined in ts_mod.py, and this should not be modified.
+
+In addition it contains the following sub-modules, which can be expanded by
+the user:
+
+read: Contains classes that are used to read grid data from different sources.
+
+Copyright 2023, Konstantinos Christakos, MET Norway
+"""
+
+from .ts_mod import TimeSeries
+from .read_metno import *
+
