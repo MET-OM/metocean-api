887f03844c11c35ead2f610b37b61f9d9c625c42
Konstantinos Christakos <67804784+KonstantinChri@users.noreply.github.com>
2023-09-19 10:15:45 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,7 @@ exclude_patterns = []
 #
 #import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
