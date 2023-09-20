496f6e8e5924ec82fb5a0dc2464fd4ee92147e9d
KonstantinChri <kchrist87@gmail.com>
2023-09-20 11:38:46 +0200

    add test

--- tests/extract_data.py
+++ tests/extract_data.py
@@ -1,7 +1,7 @@
 from metocean_api import ts
 
 # Define TimeSeries-object
-df_ts = ts.TimeSeries(lon=1.320, lat=53.324,start_time='2022-10-01', end_time='2022-12-31' , product='NORA3_wind_wave')
+df_ts = ts.TimeSeries(lon=1.320, lat=53.324,start_time='2022-11-01', end_time='2022-12-31' , product='NORA3_wind_wave')
 
 # Import data from thredds.met.no and save it as csv
 df_ts.import_data(save_csv=False)
