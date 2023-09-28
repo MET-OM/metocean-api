0ca015ddde25f42022650e73f708cad541a331ec
KonstantinChri <kchrist87@gmail.com>
2023-09-28 16:25:42 +0200

    uodate

--- metocean_api/ts/read_metno.py
+++ metocean_api/ts/read_metno.py
@@ -21,7 +21,7 @@ def NORA3_ts(self, save_csv = False):
     self.variable.append('latitude')  # keep info of regular lat
     date_list = get_date_list(product=self.product, start_date=self.start_time, end_date=self.end_time)        
 
-    tempfile = tempfile_dir(self.product,date_list,dirName="temp")
+    tempfile = tempfile_dir(self.product,date_list,dirName="temp_"+self)
 
     # extract point and create temp files
     for i in range(len(date_list)):
@@ -43,43 +43,12 @@ def NORA3_ts(self, save_csv = False):
     ds = xr.open_mfdataset(paths=tempfile)    
     #Save in csv format    
     df = create_dataframe(product=self.product,ds=ds, lon_near=lon_near, lat_near=lat_near, outfile=self.datafile, variable=self.variable[:2], start_time = self.start_time, end_time = self.end_time, save_csv=save_csv, height=self.height)    
-    del ds
+    ds.close()
     #remove temp files
     for i in range(len(date_list)):
         os.remove(tempfile[i])
     
     return df
-
-
-def NORA3_combined_ts(self, save_csv = True):
-    self.variable = ['hs','tp','tm1','tm2','tmp','Pdir','thq', 'hs_sea','tp_sea','thq_sea' ,'hs_swell','tp_swell','thq_swell']
-    self.product = 'NORA3_wave_sub'
-    df_wave = NORA3_ts(self, save_csv=True)
-    with open(self.datafile) as f:
-        top_header_wave = f.readline()
-    os.remove(self.datafile)
-    self.variable = ['wind_speed','wind_direction']
-    self.product = 'NORA3_wind_sub' 
-    df_wind = NORA3_ts(self, save_csv=True)
-    with open(self.datafile) as f:
-        top_header_wind = f.readline()
-    os.remove(self.datafile)
-    
-    # merge dataframes
-    df = df_wind.join(df_wave)
-
-    if save_csv == True:
-        df.to_csv(self.datafile, index_label='time')
-        top_header =  top_header_wave + top_header_wind + '\n'  
-        with open(self.datafile, 'r+') as f:
-            content = f.read()
-            f.seek(0, 0)
-            f.write(top_header.rstrip('\r\n') + '\n' + content)
-    
-    print('Data saved at: ' +self.datafile)
-
-    return df
-
 
 def NORA3_stormsurge_ts(self, save_csv = False):
     """
@@ -111,13 +80,43 @@ def NORA3_stormsurge_ts(self, save_csv = False):
     ds = ds.rename_vars({'ocean_time': 'time'})   
     #Save in csv format    
     df = create_dataframe(product=self.product,ds=ds, lon_near=lon_near, lat_near=lat_near, outfile=self.datafile, variable=self.variable, start_time = self.start_time, end_time = self.end_time, save_csv=save_csv, height=self.height)    
-    del ds
+    ds.close()
     #remove temp files
     for i in range(len(date_list)):
         os.remove(tempfile[i])
     
     return df
 
+
+
+def NORA3_combined_ts(self, save_csv = True):
+    self.variable = ['hs','tp','tm1','tm2','tmp','Pdir','thq', 'hs_sea','tp_sea','thq_sea' ,'hs_swell','tp_swell','thq_swell']
+    self.product = 'NORA3_wave_sub'
+    df_wave = NORA3_ts(self, save_csv=True)
+    with open(self.datafile) as f:
+        top_header_wave = f.readline()
+    os.remove(self.datafile)
+    self.variable = ['wind_speed','wind_direction']
+    self.product = 'NORA3_wind_sub' 
+    df_wind = NORA3_ts(self, save_csv=True)
+    with open(self.datafile) as f:
+        top_header_wind = f.readline()
+    os.remove(self.datafile)
+    
+    # merge dataframes
+    df = df_wind.join(df_wave)
+
+    if save_csv == True:
+        df.to_csv(self.datafile, index_label='time')
+        top_header =  top_header_wave + top_header_wind + '\n'  
+        with open(self.datafile, 'r+') as f:
+            content = f.read()
+            f.seek(0, 0)
+            f.write(top_header.rstrip('\r\n') + '\n' + content)
+    
+    print('Data saved at: ' +self.datafile)
+
+    return df
 
 def tempfile_dir(product, date_list,dirName):
     tempfile = [None] *len(date_list)
