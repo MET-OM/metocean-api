ff5a2b9c9c619770b1313926e26d1fc33b12e622
KonstantinChri <kchrist87@gmail.com>
2023-10-18 14:52:06 +0200

    update read_ec

--- examples/example_import_ERA5.py
+++ examples/example_import_ERA5.py
@@ -1,7 +1,18 @@
 from metocean_api import ts
 
 # Define TimeSeries-object
-df_ts = ts.TimeSeries(lon=1.320, lat=53.324,start_time='2000-10-01', end_time='2010-12-31' , product='ERA5')
+df_ts = ts.TimeSeries(lon=109.94, lat=15.51,start_time='2000-01-01', end_time='2010-12-31' , product='ERA5', 
+                      variable=[ 
+            'significant_height_of_combined_wind_waves_and_swell','mean_wave_direction'])
+
+# list of wind and wave parameters in ERA5:
+#        [    '100m_u_component_of_wind', '100m_v_component_of_wind', '10m_u_component_of_wind',
+#            '10m_v_component_of_wind', '2m_temperature', 'instantaneous_10m_wind_gust',
+#            'mean_direction_of_total_swell', 'mean_direction_of_wind_waves', 'mean_period_of_total_swell',
+#            'mean_period_of_wind_waves', 'mean_wave_direction', 'mean_wave_period',
+#            'peak_wave_period', 'significant_height_of_combined_wind_waves_and_swell', 'significant_height_of_total_swell',
+#            'significant_height_of_wind_waves',
+#        ]
 
 # Import data from thredds.met.no and save it as csv
 df_ts.import_data(save_csv=True)
--- metocean_api/ts/ts_mod.py
+++ metocean_api/ts/ts_mod.py
@@ -4,7 +4,7 @@ from .read_metno import *
 from .read_ec import *
 
 class TimeSeries:
-  def __init__(self, lon: float, lat: float, start_time: str='1990-01-01T00:00', end_time: str='1991-12-31T23:59',  
+  def __init__(self, lon: float, lat: float, start_time: str='1990-01-01T00:00', end_time: str='1991-12-31T23:59', variable: str=[], 
   name: str='AnonymousArea', product: str='NORA3_wave_sub', datafile: str='EmptyFile', data = [], height: int = [10, 20, 50, 100, 250, 500, 750]):
     self.name = name
     self.lon = lon
@@ -12,7 +12,7 @@ class TimeSeries:
     self.product = product
     self.start_time = start_time
     self.end_time = end_time
-    self.variable = []
+    self.variable = variable
     self.height = height
     self.datafile = product+'_lon'+str(self.lon)+'_lat'+str(self.lat)+'_'+self.start_time.replace('-','')+'_'+self.end_time.replace('-','')+'.csv'
     self.data = data
@@ -20,28 +20,36 @@ class TimeSeries:
 
   def import_data(self, save_csv = True):
     if ((self.product=='NORA3_wave_sub') or (self.product=='NORA3_wave')):
-      self.variable =  ['hs','tp','tm1','tm2','tmp','Pdir','thq', 'hs_sea','tp_sea','thq_sea' ,'hs_swell','tp_swell','thq_swell']
+      if self.variable == []:
+        self.variable =  ['hs','tp','tm1','tm2','tmp','Pdir','thq', 'hs_sea','tp_sea','thq_sea' ,'hs_swell','tp_swell','thq_swell']
+      else:
+        pass
       self.data = NORA3_wind_wave_ts(self, save_csv = save_csv)
     elif self.product == 'NORA3_wind_sub':
-       self.variable =  ['wind_speed','wind_direction']
+      if self.variable == []:       
+        self.variable =  ['wind_speed','wind_direction']
+      else:
        self.data = NORA3_wind_wave_ts(self, save_csv = save_csv)
     elif self.product == 'NORA3_wind_wave':
       self.data = NORA3_combined_ts(self, save_csv = save_csv)
     elif self.product == 'NORAC_wave':
-      self.variable =  ['hs','tp','t0m1','t02','t01','dp','dir', 'phs0','ptp0','pdir0' ,'phs1','ptp0','pdir1']
+      if self.variable == []:
+        self.variable =  ['hs','tp','t0m1','t02','t01','dp','dir', 'phs0','ptp0','pdir0' ,'phs1','ptp0','pdir1']
+      else:
+        pass
       self.data = NORAC_ts(self, save_csv = save_csv) 
     elif self.product == 'ERA5':
-      self.variable = [
-             'significant_height_of_combined_wind_waves_and_swell', 
+      if self.variable == []:
+        self.variable = [
+            '100m_u_component_of_wind', '100m_v_component_of_wind', '10m_u_component_of_wind',
+            '10m_v_component_of_wind', '2m_temperature', 'instantaneous_10m_wind_gust',
+            'mean_direction_of_total_swell', 'mean_direction_of_wind_waves', 'mean_period_of_total_swell',
+            'mean_period_of_wind_waves', 'mean_wave_direction', 'mean_wave_period',
+            'peak_wave_period', 'significant_height_of_combined_wind_waves_and_swell', 'significant_height_of_total_swell',
+            'significant_height_of_wind_waves',
         ]
-#      self.variable = [
-#            '100m_u_component_of_wind', '100m_v_component_of_wind', '10m_u_component_of_wind',
-#            '10m_v_component_of_wind', '2m_temperature', 'instantaneous_10m_wind_gust',
-#            'mean_direction_of_total_swell', 'mean_direction_of_wind_waves', 'mean_period_of_total_swell',
-#            'mean_period_of_wind_waves', 'mean_wave_direction', 'mean_wave_period',
-#            'peak_wave_period', 'significant_height_of_combined_wind_waves_and_swell', 'significant_height_of_total_swell',
-#            'significant_height_of_wind_waves',
-#        ] 
+      else:
+        pass 
       self.data = ERA5_ts(self, save_csv = save_csv) 
     elif self.product == 'NORA3_stormsurge':
       self.variable =  ['zeta']
